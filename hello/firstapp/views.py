35cc739835fae4776ca756ab8ebf7e9e84485f52
Your Name <you@example.com>
2024-09-12 12:10:45 +0300

    12.09.2024. Требовалось: выполнить Лз5. Что сделала: выполнила Лз5, конкретно - переадресацию

--- hello/firstapp/views.py
+++ hello/firstapp/views.py
@@ -2,19 +2,14 @@
 
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.http import HttpResponseRedirect
+from django.http import HttpResponsePermanentRedirect
 
 def index(request):
-    return HttpResponse("<h2>Главная</h2>")
+    return HttpResponse("Index")
 def about(request):
-    return HttpResponse("<h2>О сайте</h2>")
+    return HttpResponse("About")
 def contact(request):
-    return HttpResponse("<h2>Контакты</h2>")
-def products(request, productid):
-    category = request.GET.get("cat", "")
-    output = "<h2>Продукт № {0} Категория: {1}</h2>" .format(productid, category)
-    return HttpResponse(output)
-def users(request):
-    id = request.GET.get("id", 1)
-    name = request.GET.get("name", "Максим")
-    output = "<h2>Пользователь</h2><h3>id: {0} Имя: {1}</h3 >" .format(id, name)
-    return HttpResponse(output)
+    return HttpResponseRedirect("/about")
+def details(request):
+    return HttpResponsePermanentRedirect("/")
